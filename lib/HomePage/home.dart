793af309a8be6f4ea2638dd1b303d1366434f93c
Alvin Dinata <alvin.dinata@students.mikroskil.ac.id>
2025-04-10 20:54:12 +0700

    Update Color

--- lib/HomePage/home.dart
+++ lib/HomePage/home.dart
@@ -12,7 +12,7 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.blue,
         toolbarHeight: 70,
         leadingWidth: 700,
         leading: Padding(
