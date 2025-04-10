4c410f38612ec47c911f6e951d5c9e0fd80f3f8f
Alvin Dinata <alvin.dinata@students.mikroskil.ac.id>
2025-04-10 23:27:20 +0700

    Update Route

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const Homepage(),
+      home: const LoginPage(),
       // const LoginPage(),
     );
   }
