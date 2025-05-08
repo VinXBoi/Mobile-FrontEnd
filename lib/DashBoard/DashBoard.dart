0767a75f9beebbf516dae5a19ba333cde039b538
Alvin Dinata <alvin.dinata@students.mikroskil.ac.id>
2025-05-08 21:10:47 +0700

    Test Merge

--- lib/DashBoard/DashBoard.dart
+++ lib/DashBoard/DashBoard.dart
@@ -10,6 +10,11 @@ class DashBoardPage extends StatefulWidget {
 class _DashBoardPageState extends State<DashBoardPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Text('TEST'),
+    );
   }
 }
