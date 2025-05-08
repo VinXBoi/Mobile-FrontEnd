65fe32d94db3cb0519f8539d7dc720ce1aec8c03
Chikara2625 <veranatalia.vn05@gmail.com>
2025-05-08 17:38:55 +0700

    HomepageBaru

--- lib/LoginPage/main.dart
+++ lib/LoginPage/main.dart
@@ -1,6 +1,7 @@
-import 'package:activity_tracker/HomePage/home.dart';
-import 'package:activity_tracker/homepagebaru.dart';
-import 'package:activity_tracker/LoginPage/login.dart';
+// import 'package:activity_tracker/HomePage/home.dart';
+import 'package:activity_tracker/HomePageBaru.dart';
+// import 'package:activity_tracker/LoginPage/login.dart';
+// import 'package:activity_tracker/tes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const HomePage2(),
+      home: HomePage2(),
       // const LoginPage(),
     );
   }
