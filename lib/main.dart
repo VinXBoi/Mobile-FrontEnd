8136043eec9ef5e2af298d11cab1a535f0d63825
Alvin Dinata <alvin.dinata@students.mikroskil.ac.id>
2025-05-09 11:02:34 +0700

    UPDATE FINAL

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:activity_tracker/DashBoard/DashBoard.dart';
 import 'package:activity_tracker/LoginPage/login.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
